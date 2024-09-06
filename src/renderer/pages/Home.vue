57670ae23de493e72f6b316eeabb303b0fbfc9cf
jalzae <jalzae@gmail.com>
2024-09-06 11:03:56 +0700

    update vite styling and container

--- src/renderer/pages/Home.vue
+++ src/renderer/pages/Home.vue
@@ -1,19 +1,93 @@
 <template>
-  <div>
-    <h1 class="text-red-500 text-center">Test Router</h1>
-    <ul>
-      <li><router-link to="/main">Main</router-link></li>
-    </ul>
+  <div class="w-full">
+    <section class="bg-blue-50 dark:bg-white-900">
+      <div
+        class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0"
+      >
+        <div
+          class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700"
+        >
+          <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+            <h1
+              class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white"
+            >
+              Sign In
+            </h1>
+            <form
+              @submit.prevent="submit_form"
+              class="space-y-4 md:space-y-6"
+              action="#"
+            >
+              <div>
+                <label
+                  for="email"
+                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >Username</label
+                >
+                <input
+                  type="text"
+                  name="email"
+                  id="email"
+                  class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="name@company.com"
+                  required="true"
+                  v-model="forms.username"
+                />
+              </div>
+              <div>
+                <label
+                  for="password"
+                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >Password</label
+                >
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  placeholder="••••••••"
+                  class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  required="true"
+                  v-model="forms.password"
+                />
+              </div>
+              <div class="flex items-center justify-between"></div>
+              <button
+                type="submit"
+                class="w-full btn-primary"
+                :disabled="forms.username == '' || forms.password == ''"
+              >
+                Log in
+              </button>
+            </form>
+
+            <p v-if="error_message" class="text-danger">{{ error_message }}</p>
+          </div>
+        </div>
+      </div>
+    </section>
+
   </div>
 </template>
 <script lang='ts'>
-import { defineComponent } from 'vue'
+import { defineComponent,computed } from 'vue'
 export default defineComponent({
-  setup() { },
+  
   mixins: [],
   components: {},
   data() {
-    return {}
+    return {
+    forms: { username: "", password: "", role: 2 },
+      error_message: "",
+      form: [
+        {
+          model: "username",
+          validation: ["required", "noSpace"],
+        },
+        {
+          model: "password",
+          validation: ["required", "noSpace"],
+        },
+      ],}
   },
   methods: {},
   mounted() { },
